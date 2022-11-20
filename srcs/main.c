274d498bed6cf22f4e1a1a7a398c929411d64a18
Tag6362 <tag6362@gmail.com>
2022-11-20 22:21:50 +0100

    twt

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,10 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 15:53:46 by tgernez           #+#    #+#             */
-/*   Updated: 2022/11/19 19:07:28 by tgernez          ###   ########.fr       */
+/*   Updated: 2022/11/19 20:40:59 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
-#include "libft.h"
 #include "fdf.h"
 
 int main(void)
